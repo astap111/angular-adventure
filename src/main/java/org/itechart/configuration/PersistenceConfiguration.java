2d02a2943ba478affd1d43252ae2b90574e67c28
artsiom.astapenka <astap111@gmail.com>
2016-04-06 17:53:59 +0300

    added postgre hiber dialect

--- src/main/java/org/itechart/configuration/PersistenceConfiguration.java
+++ src/main/java/org/itechart/configuration/PersistenceConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -39,6 +40,7 @@ public class PersistenceConfiguration {
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
 
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.POSTGRESQL);
         vendorAdapter.setShowSql(true);
         vendorAdapter.setGenerateDdl(true);
 
