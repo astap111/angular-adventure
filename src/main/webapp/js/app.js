4e5588926704ad6d99538d364e5537a7ae69064e
artsiom.astapenka <astap111@gmail.com>
2016-04-13 12:08:34 +0300

    authService.checkRights() corrected and completed

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('userApp', ['ui.router']);
 
 app.run([
-    '$rootScope', '$state', '$stateParams', '$http', 'authServise',
-    function ($rootScope, $state, $stateParams, $http, authServise) {
+    '$rootScope', '$state', '$stateParams', '$http', 'authService',
+    function ($rootScope, $state, $stateParams, $http, authService) {
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
 
@@ -11,36 +11,50 @@ app.run([
             .then(function (response) {
                 $rootScope.loggedUser = response.data;
                 $rootScope.loggedUserRoles = $rootScope.loggedUser.roles;
+                $rootScope.authService = authService;
 
                 $rootScope.$on('$stateChangeStart',
                     function (event, toState, toParams, fromState, fromParams) {
-                        if (!authServise.checkAccess(event, toState, toParams, fromState, fromParams)) {
+                        if (!authService.checkAccess(event, toState, toParams, fromState, fromParams)) {
                             event.preventDefault();
                             $state.go('home');
                         }
                     }
                 );
             });
+
     }
 ]);
 
-app.service('authServise', function ($rootScope) {
+app.service('authService', function ($rootScope) {
     this.checkAccess = function (event, toState, toParams, fromState, fromParams) {
         if (toState.data !== undefined && toState.data.permitTo) {
             var permitTo = toState.data.permitTo;
             var currentRoles = $rootScope.loggedUserRoles;
-            var permitAccess = false;
 
-            for (var i = 0; i < permitTo.length; i++) {
-                for (var j = 0; j < currentRoles.length; j++) {
-                    if (permitTo[i] === currentRoles[j].roleName) {
-                        return true;
-                    }
-                }
-            }
-            return permitAccess;
+            return _checkIntersection(currentRoles, permitTo);
         } else {
             return true;
         }
+    };
+
+    this.checkRights = function (permitTo) {
+        var currentRoles = $rootScope.loggedUserRoles;
+        if (permitTo) {
+            return _checkIntersection(currentRoles, permitTo);
+        }
+    };
+
+    _checkIntersection = function (currentRoles, permitTo) {
+        var permitAccess = false;
+
+        for (var i = 0; i < permitTo.length; i++) {
+            for (var j = 0; j < currentRoles.length; j++) {
+                if (permitTo[i] === currentRoles[j].roleName) {
+                    return true;
+                }
+            }
+        }
+        return permitAccess;
     }
 });
